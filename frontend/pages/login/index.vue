a83b7ecc4c965ab9d8c7de178b7fe19ff63bb1b7
Christian Martin Cabucos <christianmartincabucos@gmail.com>
2024-06-25 08:48:14 +0800

    remove vuetify logo

--- frontend/pages/login/index.vue
+++ frontend/pages/login/index.vue
@@ -36,9 +36,7 @@ const handleLogin = async () => {
 </script>
 <template>
   <div>
-      <v-img class="mx-auto my-6" max-width="228"
-        src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"></v-img>
-      <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="500" rounded="lg">
+      <v-card class="mx-auto pa-12 pb-8 my-6" elevation="8" max-width="500" rounded="lg">
         <v-alert
           border="top"
           type="warning"
